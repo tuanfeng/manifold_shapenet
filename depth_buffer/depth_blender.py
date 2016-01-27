2d5e688fb3fc1d4b4b75093a223d15138768867b
haosu <haosu@cs.stanford.edu>
2016-01-27 10:43:17 -0800

    initial check

---
+++ depth_buffer/depth_blender.py
@@ -0,0 +1,149 @@
+#   blender -b depth.blend -P depth_blender.py > log
+
+import bpy
+import os
+import sys
+import math
+from shutil import move as shutil_move
+from numpy import cross as np_cross
+from numpy import array as np_array
+from numpy import linalg as np_linalg
+from subprocess import Popen
+
+#change scene name to yours
+Scenename = 'Scene'
+
+task_info=open('task_info','r')
+#task_info=open('/home/tuanfeng/Documents/smart_texture/code/depth_buffer/task_info','r')
+#task_info=open('/Users/tuanfeng/Documents/ResearchWork/project_2/code/depth_buffer/task_info_mac','r')
+
+task_info_ = task_info.read().splitlines();
+print("task info: ", task_info_)
+
+bpy.data.scenes[Scenename].render.resolution_x = int(task_info_[3])
+bpy.data.scenes[Scenename].render.resolution_y = int(task_info_[4])
+bpy.data.scenes[Scenename].render.resolution_percentage = 100
+#bpy.data.scenes[Scenename].render.image_settings.file_format = 'PNG'
+bpy.data.scenes[Scenename].render.image_settings.quality = 100
+#bpy.data.scenes[Scenename].render.image_settings.color_mode = 'RGB'
+bpy.data.scenes[Scenename].render.use_stamp = 0
+bpy.data.scenes[Scenename].render.use_placeholder = True
+bpy.data.scenes[Scenename].render.use_overwrite = True
+bpy.context.scene.render.engine = "BLENDER_RENDER"
+
+
+bpy.data.scenes[0].node_tree.nodes[3].base_path = task_info_[1]+'/tmp_c1/'
+bpy.data.scenes[0].node_tree.nodes[4].base_path = task_info_[1]+'/tmp_d1/'
+
+
+
+#bpy.data.scenes[Scenename].cycles.samples = int(task_info_[5])
+
+bpy.data.scenes[Scenename].render.use_antialiasing = True
+bpy.data.scenes[Scenename].render.use_full_sample = True
+
+bpy.ops.import_scene.obj(filepath=task_info_[0],use_split_groups=False)
+obj_name = os.path.splitext(os.path.basename(task_info_[0]))[0]
+
+if not os.path.exists(task_info_[1]+'/'+obj_name+'_cd/'): os.makedirs(task_info_[1]+'/'+obj_name+'_cd/')
+
+#bpy.data.objects[obj_name].rotation_euler = (0,0,0)
+
+vertex_avg_x = float(0)
+vertex_avg_y = float(0)
+vertex_avg_z = float(0)
+
+vertex_min_x = float("+inf")
+vertex_min_y = float("+inf")
+vertex_min_z = float("+inf")
+vertex_max_x = float("-inf")
+vertex_max_y = float("-inf")
+vertex_max_z = float("-inf")
+
+vertex_num = float(len(bpy.data.meshes[obj_name].vertices))
+
+vert_dis = float(0)
+
+#centering
+for vertex in bpy.data.meshes[obj_name].vertices:
+	vertex_avg_x = vertex_avg_x + vertex.co[0] / vertex_num
+	vertex_avg_y = vertex_avg_y + vertex.co[1] / vertex_num
+	vertex_avg_z = vertex_avg_z + vertex.co[2] / vertex_num
+	vertex_min_x = min(vertex_min_x,vertex.co[0])
+	vertex_min_y = min(vertex_min_y,vertex.co[1])
+	vertex_min_z = min(vertex_min_z,vertex.co[2])
+	vertex_max_x = max(vertex_max_x,vertex.co[0])
+	vertex_max_y = max(vertex_max_y,vertex.co[1])
+	vertex_max_z = max(vertex_max_z,vertex.co[2])
+
+for vertex in bpy.data.meshes[obj_name].vertices:
+	vertex.co[0] = (vertex.co[0] - vertex_avg_x)
+	vertex.co[1] = (vertex.co[1] - vertex_avg_y)
+	vertex.co[2] = (vertex.co[2] - vertex_avg_z)
+	vert_dis = max(vert_dis, math.pow(math.pow(vertex.co[0],2)+math.pow(vertex.co[1],2)+math.pow(vertex.co[2],2),0.5))
+
+#scaling
+for vertex in bpy.data.meshes[obj_name].vertices:
+	vertex.co[0] = vertex.co[0]/vert_dis/1.01
+	vertex.co[1] = vertex.co[1]/vert_dis/1.01
+	vertex.co[2] = vertex.co[2]/vert_dis/1.01
+
+for material_ in bpy.data.materials:
+	bpy.data.materials[material_.name].use_shadeless = True
+	bpy.data.materials[material_.name].use_transparency = False
+
+S = int(task_info_[5])
+
+Rad = float(5.0)
+
+sp_vert_num = 0
+
+if os.path.isfile(task_info_[2]+'/'+obj_name+'.off'):
+	os.remove(task_info_[2]+'/'+obj_name+'.off')
+
+for i in range(1, 2 * S + 1):
+	ui = math.asin(1 - float(2*i-1)/float(2*S))
+	vi = ui * math.pow(2*float(S)*math.pi, 0.5)
+	ni = [math.cos(ui)*math.cos(vi), math.cos(ui)*math.sin(vi), math.sin(ui)]
+	
+	bpy.data.objects['Camera'].location[0] = ni[0] * Rad
+	bpy.data.objects['Camera'].location[1] = ni[1] * Rad
+	bpy.data.objects['Camera'].location[2] = ni[2] * Rad
+
+	cross_product = np_cross(np_array([0,0,-ni[2]]),np_array([-ni[0],-ni[1],-ni[2]]))
+	la = np_linalg.norm(cross_product)
+
+	bpy.data.objects['Camera'].rotation_axis_angle[1] = cross_product[0] / la 
+	bpy.data.objects['Camera'].rotation_axis_angle[2] = cross_product[1] / la 
+	bpy.data.objects['Camera'].rotation_axis_angle[3] = cross_product[2] / la
+
+	bpy.data.objects['Camera'].rotation_axis_angle[0] = math.atan(math.pow(math.pow(ni[0],2)+math.pow(ni[1],2),0.5)/math.fabs(ni[2]))
+	if ni[2] <= 0 : bpy.data.objects['Camera'].rotation_axis_angle[0] = math.pi + bpy.data.objects['Camera'].rotation_axis_angle[0]
+
+	bpy.ops.render.render(animation=False,write_still=True,scene=Scenename)
+
+	f = open('render_report', 'w')
+	f.write(str(bpy.data.objects['Camera'].location[0])+' '+str(bpy.data.objects['Camera'].location[1])+' '+str(bpy.data.objects['Camera'].location[2])+'\n')
+	f.write(str(bpy.data.objects['Camera'].rotation_axis_angle[0])+' '+str(bpy.data.objects['Camera'].rotation_axis_angle[1])+' '+str(bpy.data.objects['Camera'].rotation_axis_angle[2])+' '+str(bpy.data.objects['Camera'].rotation_axis_angle[3])+'\n')
+	f.close()
+
+	process_sp = Popen("python process_sp.py".split())
+	process_sp.communicate()
+
+	shutil_move(task_info_[1]+'/tmp_c1/Image0001.png',task_info_[1]+'/'+obj_name+'_cd/'+obj_name+'_'+str(i).zfill(2)+'_c.png')
+	shutil_move(task_info_[1]+'/tmp_d1/Image0001.png',task_info_[1]+'/'+obj_name+'_cd/'+obj_name+'_'+str(i).zfill(2)+'_d.png')
+
+
+
+with open(task_info_[2]+'/'+obj_name+'.off','r') as file:
+	data = file.readlines()
+
+f = open(task_info_[2]+'/'+obj_name+'.off','w')
+f.write('COFF\n')
+f.write(str(len(data))+' 0 0\n')
+f.writelines(data)
+f.close()
+
+
+
+
---
+++ depth_buffer/process_sp.py
@@ -0,0 +1,97 @@
+from PIL import Image
+from numpy import array as np_array
+from numpy import asarray as np_asarray
+from numpy import linalg as np_linalg
+from numpy import eye as np_eye
+import os, math
+
+task_info=open('task_info','r')
+#task_info=open('/home/tuanfeng/Documents/smart_texture/code/depth_buffer/task_info','r')
+
+task_info_ = task_info.read().splitlines()
+
+obj_name = os.path.splitext(os.path.basename(task_info_[0]))[0]
+
+render_info = open('render_report', 'r')
+render_info_ =  render_info.read().splitlines()
+
+camera_pos_x = float(render_info_[0].split()[0])
+camera_pos_y = float(render_info_[0].split()[1])
+camera_pos_z = float(render_info_[0].split()[2])
+
+#print camera_pos_x,' ', camera_pos_y,' ',camera_pos_z
+
+dir_vector_x = -camera_pos_x / np_linalg.norm([-camera_pos_x, -camera_pos_y, -camera_pos_z])
+dir_vector_y = -camera_pos_y / np_linalg.norm([-camera_pos_x, -camera_pos_y, -camera_pos_z])
+dir_vector_z = -camera_pos_z / np_linalg.norm([-camera_pos_x, -camera_pos_y, -camera_pos_z])
+
+camera_rot_w = float(render_info_[1].split()[0])
+camera_rot_x = float(render_info_[1].split()[1])
+camera_rot_y = float(render_info_[1].split()[2])
+camera_rot_z = float(render_info_[1].split()[3])
+
+#print camera_rot_w,' ',camera_rot_x,' ',camera_rot_y,' ',camera_rot_z
+
+cosi = math.cos(camera_rot_w)
+sini = math.sin(camera_rot_w)
+ux = camera_rot_x
+uy = camera_rot_y
+uz = camera_rot_z
+
+Rot = np_eye(3)
+Rot[0][0] = cosi+ux*ux*(1-cosi)
+Rot[0][1] = ux*uy*(1-cosi)-uz*sini
+Rot[0][2] = ux*uz*(1-cosi)+uy*sini
+Rot[1][0] = uy*ux*(1-cosi)+uz*sini
+Rot[1][1] = cosi+uy*uy*(1-cosi)
+Rot[1][2] = uy*uz*(1-cosi)-ux*sini
+Rot[2][0] = uz*ux*(1-cosi)-uy*sini
+Rot[2][1] = uz*uy*(1-cosi)+ux*sini
+Rot[2][2] = cosi+uz*uz*(1-cosi)
+
+dir_up = [Rot[0][1],Rot[1][1],Rot[2][1]]
+dir_ri = [Rot[0][0],Rot[1][0],Rot[2][0]]
+#print np_linalg.norm(dir_up), np_linalg.norm(dir_ri)
+#print(dir_up[0]*dir_ri[0]+dir_up[1]*dir_ri[1]+dir_up[2]*dir_ri[2])
+
+image_c_path = task_info_[1]+'/tmp_c1/Image0001.png'
+image_d_path = task_info_[1]+'/tmp_d1/Image0001.png'
+image_c_ori = Image.open(image_c_path, 'r')
+image_d_ori = Image.open(image_d_path, 'r')
+image_c = np_asarray(image_c_ori)
+image_d = np_asarray(image_d_ori)
+
+sp_file_path = task_info_[2]+'/'+obj_name+'.off'
+
+fout = open(sp_file_path,'a')
+
+for pi in range(0,image_c_ori.size[0]):
+	for pj in range(0,image_c_ori.size[1]):
+		
+		imgd = image_d[pi,pj]
+
+	#	if imgd > 0 and imgd < 32760:
+	#		print(imgd)
+	#	imgd = 65535
+	#	if pi>200 and pi<300 and pj>200 and pj<300:
+	#		imgd = 1
+	#	if pi>300 and pi<400 and pj>300 and pj<400:
+	#		imgd = 65534
+	#	if pi>200 and pi<300 and pj>300 and pj<400:
+	#		imgd = 32767
+	#	if pi>300 and pi<400 and pj>200 and pj<300:
+	#		imgd = 32767
+		if imgd > 0 and imgd < 65535:
+			pixel_color = image_c[pi,pj]
+			pixel_depth = float(4.0) + float(imgd)/float(65535)*float(2.0)
+			pixel_plan_up = (-float(pi)+float(image_c_ori.size[0])/float(2))/(float(image_c_ori.size[0])/float(2))
+			pixel_plan_ri = (+float(pj)-float(image_c_ori.size[1])/float(2))/(float(image_c_ori.size[1])/float(2))
+			pixel_plan_pos_x = pixel_plan_up * dir_up[0] + pixel_plan_ri * dir_ri[0]
+			pixel_plan_pos_y = pixel_plan_up * dir_up[1] + pixel_plan_ri * dir_ri[1]
+			pixel_plan_pos_z = pixel_plan_up * dir_up[2] + pixel_plan_ri * dir_ri[2]
+			real_pos_x = camera_pos_x + pixel_plan_pos_x + pixel_depth * dir_vector_x
+			real_pos_y = camera_pos_y + pixel_plan_pos_y + pixel_depth * dir_vector_y
+			real_pos_z = camera_pos_z + pixel_plan_pos_z + pixel_depth * dir_vector_z
+			fout.write(str(real_pos_x)+' '+str(real_pos_y)+' '+str(real_pos_z)+' '+str(pixel_color[0])+' '+str(pixel_color[1])+' '+str(pixel_color[2])+'\n')
+
+fout.close()
